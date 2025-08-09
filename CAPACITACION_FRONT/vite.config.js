d97c7e6d986cbe00e39cb320d3f282256db6a201
Taciturno11 <martinsitonauca@gmail.com>
2025-08-09 15:49:30 -0500

    Nuevos cambios

--- CAPACITACION_FRONT/vite.config.js
+++ CAPACITACION_FRONT/vite.config.js
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',      // <-- ¡Agrega esto!
+    port: 5173,           // <-- ¡Y esto si quieres el puerto por defecto!
     proxy: {
       '/api': 'http://localhost:3001',
     },
